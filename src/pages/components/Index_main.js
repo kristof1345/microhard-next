027271b3de5da826652eb63aa8a13270db9ddd64
kristof1345 <kristofnemeth1345@gmail.com>
2023-04-19 14:04:56 +0200

    commit

--- src/pages/components/Index_main.js
+++ src/pages/components/Index_main.js
@@ -2,6 +2,8 @@ import React from "react";
 import Windows from "../../../public/windows.webp";
 import Shop from "../../../public/shop.webp";
 import BShop from "../../../public/shop-business.webp";
+import { Posts1 } from "../data/posts1";
+import edge from "../../../public/edge.avif";
 
 const Index_main = () => {
   return (
@@ -13,12 +15,22 @@ const Index_main = () => {
         </div>
         <div className="item-2">
           <img src={Shop.src} alt="shop" />
-          <div>Shop</div>
+          <div>Shop here</div>
         </div>
         <div className="item-3">
           <img src={BShop.src} alt="business shop" />
           <div>Business Shop</div>
         </div>
+      </div>
+      <div className="posts-sec">
+        {Posts1.map((post, i) => (
+          <div className="post" key={i}>
+            <img src={edge.src} alt="post image" />
+            <h4>{post.title}</h4>
+            <p>{post.description}</p>
+            <button>Shop Now</button>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/pages/data/posts1.js
@@ -0,0 +1,28 @@
+const Posts1 = [
+  {
+    title: "Microhard Edge",
+    description:
+      "Turn your ideas into reality, stay safer online and off, and focus on what matters most with Microsoft 365 apps.",
+    imageSrc: "../../../public/edge.avif",
+  },
+  {
+    title: "Microhard Edge",
+    description:
+      "Turn your ideas into reality, stay safer online and off, and focus on what matters most with Microsoft 365 apps.",
+    imageSrc: "../../../public/edge.avif",
+  },
+  {
+    title: "Microhard Edge",
+    description:
+      "Turn your ideas into reality, stay safer online and off, and focus on what matters most with Microsoft 365 apps.",
+    imageSrc: "../../../public/edge.avif",
+  },
+  {
+    title: "Microhard Edge",
+    description:
+      "Turn your ideas into reality, stay safer online and off, and focus on what matters most with Microsoft 365 apps.",
+    imageSrc: "../../../public/edge.avif",
+  },
+];
+
+export { Posts1 };
