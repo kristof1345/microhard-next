117dfffcf7d76e4e68312c5f604b7034a0f1bbbf
kristof1345 <kristofnemeth1345@gmail.com>
2023-04-21 23:27:25 +0200

    bing-page

---
+++ src/pages/components/card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const card = () => {
+  return (
+    <div className="card">
+      <div className="over">Help me plan my special trip</div>
+      <div className="under">
+        I am planning a trip for our anniversary in September. What are some
+        places we can go that are within a 3 hour flight from London Heathrow?
+      </div>
+    </div>
+  );
+};
+
+export default card;
